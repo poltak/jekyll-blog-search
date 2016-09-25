db2ab44a13f6eb3574957d4def40af11b0c9c7df
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2016-09-25 07:49:58 +0700

    Made sure entrypoint expects array of posts
    - deserialisation should now be done before entrypoint required
    - updated init in dev HTML

--- app/initialize.tsx
+++ app/initialize.tsx
@@ -1,7 +1,9 @@
 import { render } from 'react-dom';
 import * as React from 'react';
 import BlogSearch from './containers/BlogSearch';
+import { PostType } from './util/postFilter';
 
-document.addEventListener('DOMContentLoaded', () => {
-  render(<BlogSearch />, document.getElementById('#jekyll-blog-search'));
-});
+export default (initPosts: PostType[]) =>
+  document.addEventListener('DOMContentLoaded', () => {
+    render(<BlogSearch initPosts={initPosts} />, document.getElementById('#jekyll-blog-search'));
+  });
